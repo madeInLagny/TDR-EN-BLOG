ded6204a78d4b379b62f7de32a666d02d5faaf74
Jocelyn <dev@madeinlagny.com>
2024-10-27 12:59:21 +0100

    tawkto added to blog

---
+++ assets/js/tawkto.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6527b233eb150b3fb9a0a8a6/1hchh06rt';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
